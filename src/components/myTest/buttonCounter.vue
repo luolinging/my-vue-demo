720f4bfef1290bb7c3b5babbc45f226a0239617a
luolinging <1968967624@qq.com>
2018-03-09 21:20:33 +0800

    修改父子组件的自定义事件通信

---
+++ src/components/myTest/buttonCounter.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<div class="counter child">
+			<button v-on:click="incrementCounter">{{ counter }}</button>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				counter: 0
+			}
+		},
+		methods: {
+			incrementCounter() {
+				this.counter += 1;
+				this.$emit('increment');
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
--- src/components/parentVue.vue
+++ src/components/parentVue.vue
@@ -1,18 +1,28 @@
 <template>
-	<div class="parent-div">
-		<!--使用声明的组件:子组件-->
-		<!--<header-vue>这种写法是在angular.js中使用的，vue.js中也可以写成headerVue-->
-		<header-vue textone="---父组件携带的常量"></header-vue>
-		<!--常量值-->
-		<body-vue v-bind:texttwo="text"></body-vue>
-		<!--变量值-->
-		<footer-vue></footer-vue>
-		<button @click="listen">爸爸接听电话</button>
+	<div>
+		<div class="parent-div">
+			<!--使用声明的组件:子组件-->
+			<!--<header-vue>这种写法是在angular.js中使用的，vue.js中也可以写成headerVue-->
+			<header-vue textone="---父组件携带的常量"></header-vue><!--常量值-->
+			<body-vue v-bind:texttwo="texttwo"></body-vue><!--变量值-->
+			<!--使用自定义事件实现父子组件间的通信-->
+			<footer-vue></footer-vue>
+			<button @click="listen">爸爸接听电话</button>
+		</div>
+
+		<div class="new parent counter">
+			<p>{{ total }}</p>
+			<!--父组件在使用子组件的地方直接用 v-on 来监听子组件触发的事件-->
+			<button-counter v-on:increment="incrementTotal"></button-counter>
+			<button-counter @increment="incrementTotal"></button-counter>
+		</div>
+
 	</div>
 </template>
 
 <script>
 	import connect from '@/js/connect.js'
+	import buttonCounter from './myTest/buttonCounter.vue'
 	/*//引入子组件对象
 	import header from './myTest/headerVue.vue';
 	import body from './myTest/bodyVue.vue';
@@ -21,7 +31,9 @@
 	export default {
 		data() {
 			return {
-				text: '---父组件携带的变量'
+				texttwo: '---父组件携带的变量',
+				total: 0
+
 			}
 		},
 		methods: {
@@ -30,9 +42,12 @@
 				connect.$on('phone', function(msg) {
 					console.log(msg);
 				})
+			},
+			incrementTotal() {
+				this.total += 1;
 			}
 
-		}
+		},
 		/*//必须声明组件
 		components: {
 			//组件名（在模板中使用）：组件对象
@@ -40,6 +55,9 @@
 			body, //对应上面的简写
 			footer
 		}*/
+		components: {
+			buttonCounter: buttonCounter
+		}
 	}
 </script>
 
