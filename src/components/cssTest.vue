c19d5ca7c26811f8c5a6ee78134eaaba6a73f456
luolinging <1968967624@qq.com>
2018-03-13 13:44:11 +0800

    生成一个旋转的立方体（css3）

--- src/components/cssTest.vue
+++ src/components/cssTest.vue
@@ -1,14 +1,41 @@
 <template>
-	<div class="test 2d">
-		<img src="../assets/setting.png"/>
+	<div>
+		<div class="test">
+			<img src="../assets/setting.png" />
+		</div>
+		<div class="cube">
+			<cube></cube>
+		</div>
+
 	</div>
 </template>
 
 <script>
+	import cube from './cube'
+
+	export default {
+		components: {
+			cube
+		}
+	}
 </script>
 
 <style>
-	.2d:hover img{
-		transform: rotateX(100deg);
+	img {
+		width: 50px;
+		height: 50px;
+		margin: 20px auto;
+		transition: all 2s;/*播放的速度*/
+	}
+	
+	.test {
+		margin-left: -600px;
+		width: 100%;
+		height: 100%;
+		perspective: 800px;/*辅助作用    产生近大远小的感觉*/
+	}
+	
+	.test:hover img {
+		transform: translateZ(200px);/*沿着Z轴移动200px*/
 	}
 </style>
---
+++ src/components/cube.vue
@@ -0,0 +1,79 @@
+<template>
+	<!--生成一个立方体-->
+	<div class="cube">
+		<div class="front">front</div>
+		<div class="back">back</div>
+		<div class="left">left</div>
+		<div class="right">right</div>
+		<div class="top">top</div>
+		<div class="bottom">bottom</div>
+	</div>
+</template>
+
+<script>
+</script>
+
+<style>
+	.cube {
+		width: 200px;
+		height: 200px;
+		margin: 50px auto;
+		position: relative;
+		/*perspective: 1000px;*/
+		/*opacity: 0.5;透明度*/
+		transition: all 8s;
+		transform-style: preserve-3d;/*将平面转换成一个立方体*/
+		transform: rotateX(30deg) rotateY(60deg);
+		
+	}
+
+	.front,
+	.back,
+	.left,
+	.right,
+	.top,
+	.bottom {
+		width: 100%;
+		height: 100%;
+		text-align: center;
+		line-height: 200px;
+		font-size: 20px;
+		position: absolute;
+		left: 0;
+		top: 0;
+	}
+	
+	.front {
+		background-color: green;
+		transform: translateZ(-100px);
+	}
+	
+	.back {
+		background-color: aquamarine;
+		transform: translateZ(100px);
+	}
+	
+	.left {
+		background-color: red;
+		transform: rotateY(90deg) translateZ(-100px);
+	}
+	
+	.right {
+		background-color: royalblue;
+		transform: rotateY(90deg) translateZ(100px);
+	}
+	
+	.top {
+		background-color: gold;
+		transform: rotateX(90deg) translateZ(-100px);
+	}
+	
+	.bottom {
+		background-color: chartreuse;
+		transform: rotateX(90deg) translateZ(100px);
+	}
+	
+	.cube:hover{
+		transform: rotateX(360deg) rotateY(360deg);
+	}
+</style>
